905f4f8118c45baa94f8d1a97049f1b4b120a0ee
Roshan <roshanbhuvad15@gmail.com>
2020-07-26 15:47:03 +0530

    fetched Instagram API

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,44 @@
 // Full Documentation - https://docs.turbo360.co
+const turbo = require("turbo360")({ site_id: process.env.TURBO_APP_ID });
+const vertex = require("vertex360")({ site_id: process.env.TURBO_APP_ID });
 const express = require("express");
 const router = express.Router();
+const superagent = require("superagent");
 
-/*  This is the home route. It renders the index.mustache page from the views directory.
-	Data is rendered using the Mustache templating engine. For more
-	information, view here: https://mustache.github.io/#demo */
 router.get("/", (req, res) => {
-  res.render("index", {
-    text:
-      "This is the dynamic data. Open index.js from the routes directory to see.",
-  });
+  const data = {
+    text: "Instagram Clone!",
+    greeting: "Welcome !",
+  };
+  res.render("index", data);
 });
 
+router.get("/:username", (req, res) => {
+  const username = req.params.username;
+  const instagramAPI = "https://www.instagram.com/" + username + "/?__a=1";
+
+  superagent
+    .get(instagramAPI)
+    .query(null)
+    .set("Accept", "application/json")
+    .end((err, response) => {
+      if (err) {
+        res.json({
+          confirmation: "fail",
+          message: err.message,
+        });
+        return;
+      }
+      res.render("index", response.body);
+      /*res.json({
+        confirmation: "success",
+        data: response.body, */
+    });
+});
+/* res.json({
+    confirmation: "success",
+    username: username,
+  }); */
 /*  This route render json data 
 router.get("/json", (req, res) => {
   res.json({
