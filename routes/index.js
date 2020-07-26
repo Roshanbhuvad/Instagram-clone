3765ff52b3cc0a149e31e14f78505a0ced278394
Roshan <roshanbhuvad15@gmail.com>
2020-07-26 18:45:30 +0530

    Successfully rendered Instagram accounts

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,4 @@
 // Full Documentation - https://docs.turbo360.co
-const turbo = require("turbo360")({ site_id: process.env.TURBO_APP_ID });
-const vertex = require("vertex360")({ site_id: process.env.TURBO_APP_ID });
 const express = require("express");
 const router = express.Router();
 const superagent = require("superagent");
@@ -32,31 +30,8 @@ router.get("/:username", (req, res) => {
       res.render("index", response.body);
       /*res.json({
         confirmation: "success",
-        data: response.body, */
+        data: response.body,
+      }); */
     });
 });
-/* res.json({
-    confirmation: "success",
-    username: username,
-  }); */
-/*  This route render json data 
-router.get("/json", (req, res) => {
-  res.json({
-    confirmation: "success",
-    app: process.env.TURBO_APP_ID,
-    data: "this is a sample json route.",
-  });
-});
-
- This route sends text back as plain text. 
-router.get('/send', (req, res) => {
-	res.send('This is the Send Route')
-})
-
-  This route redirects requests to Turbo360. 
-router.get('/redirect', (req, res) => {
-	res.redirect('https://www.turbo360.co/landing')
-})
-*/
-
 module.exports = router;
