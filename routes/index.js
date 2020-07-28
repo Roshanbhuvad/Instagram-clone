3c90d8a9001314be94a3483a6b3f4a55eef0a15b
Roshan <roshanbhuvad15@gmail.com>
2020-07-28 23:05:00 +0530

    superagent and bluebird promises

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
-// Full Documentation - https://docs.turbo360.co
+const turbo = require("turbo360")({ site_id: process.env.TURBO_APP_ID });
+const vertex = require("vertex360")({ site_id: process.env.TURBO_APP_ID });
 const express = require("express");
 const router = express.Router();
-const superagent = require("superagent");
+//const superagent = require("superagent");
+const utils = require("./utils");
 
 router.get("/", (req, res) => {
   const data = {
@@ -15,28 +17,28 @@ router.get("/:username", (req, res) => {
   const username = req.params.username;
   const instagramAPI = "https://www.instagram.com/" + username + "/?__a=1";
 
-  superagent
-    .get(instagramAPI)
-    .query(null)
-    .set("Accept", "application/json")
-    .end((err, response) => {
-      if (err) {
-        const data = {
-          message: err.message || "Check you Spelling!",
-        };
-        res.render("error", data);
-        /*res.json({
+  utils.HTTP.get(instagramAPI, null)
+    .then((data) => {
+      res.render("index", data);
+    })
+    .catch((err) => {
+      const data = {
+        message: err.message || "Check you Spelling!",
+      };
+      res.render("error", data);
+    });
+
+  /*res.json({
           confirmation: "fail",
           message: err.message,
-        }); */
+        }); 
         return;
       }
-      res.render("index", response.body);
-      /*res.json({
+      res.render("index", response.body); */
+  /*res.json({
         confirmation: "success",
         data: response.body,
       }); */
-    });
 });
 
 router.get("/:username/:postcode", (req, res) => {
@@ -45,7 +47,37 @@ router.get("/:username/:postcode", (req, res) => {
 
   const instagramAPI = "https://www.instagram.com/" + username + "/?__a=1";
 
-  superagent
+  utils.HTTP.get(instagramAPI, null)
+    .then((data) => {
+      const user = data.graphql.user;
+      const posts = user.edge_owner_to_timeline_media.edges;
+      let selectedPost = null;
+      for (let i = 0; i < posts.length; i++) {
+        const post = posts[i];
+        if (post.node.shortcode == postcode) {
+          selectedPost = post;
+          break;
+        }
+      }
+      if (selectedPost == null) {
+        throw new Error("Post not found!");
+        return;
+      }
+      selectedPost["user"] = {
+        username: user.username,
+        icon: user.profile_pic_url,
+      };
+      res.render("post", selectedPost);
+      return;
+    })
+    .catch((err) => {
+      const data = {
+        message: err.message || "Check you Spelling!",
+      };
+      res.render("error", data);
+      return;
+    });
+  /*superagent
     .get(instagramAPI)
     .query(null)
     .set("Accept", "application/json")
@@ -78,10 +110,10 @@ router.get("/:username/:postcode", (req, res) => {
         //caption: caption.text,
       };
       res.render("post", selectedPost);
-      /*res.json({
+      res.json({
         confirmation: "success",
         data: selectedPost,
-      }); */
-    });
+      }); 
+    }); */
 });
 module.exports = router;
--- routes/utils/HTTP.js
+++ routes/utils/HTTP.js
@@ -1,5 +1,25 @@
 const superagent = require("superagent");
+const Promise = require("bluebird");
 
 module.exports = {
-  get: (url, query) => {},
+  get: (url, query) => {
+    return new Promise((resolve, reject) => {
+      superagent
+        .get(url)
+        .query(query)
+        .set("Accept", "application/json")
+        .end((err, response) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          /*res.json({
+          confirmation: "fail",
+          message: err.message,
+        }); */
+          resolve(response.body);
+          // res.render("index", response.body);
+        });
+    });
+  },
 };
